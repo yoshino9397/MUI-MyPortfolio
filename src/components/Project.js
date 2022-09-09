28044e069180fdea582e3b01f0beb49b8e562bd7
yoshino9397 <yosino8512@gmail.com>
2022-09-09 15:39:46 -0700

    project updated

--- src/components/Project.js
+++ src/components/Project.js
@@ -15,56 +15,56 @@ import Link from "@mui/material/Link";
 const projects = [
   {
     name: "Material-UI Portfolio",
-    description: `I have developed a simple but dynamic React project, mainly using React and Material UI.Created Responsive User Experience with MUI Grids that allows the user to view any of my works which are organized.`,
+    description: `I have developed a simple but dynamic React project, mainly using React and Material UI.Created Responsive User Experience with MUI Grids that allows the user to view any of my works that are organized.`,
     image: "https://live.staticflickr.com/65535/52005591590_91fa918598_k.jpg",
     url: "https://yoshino-mui-portfolio.netlify.app/",
     code: "https://github.com/yoshino9397/MUI-MyPortfolio",
   },
   {
     name: "Time tracking App",
-    description: `Time tracking app with Pomodoro technique based on Toggle, co-developed with a friend.You can manage tasks divided by project and check the percentage of time spent on activities and tasks using a timer, output reports on activity history in CSV.`,
+    description: `Time tracking app with Pomodoro technique based on Toggle, co-developed with a friend. You can manage tasks divided by project and check the percentage of time spent on activities and tasks using a timer, and output reports on activity history in CSV.`,
     image: "https://live.staticflickr.com/65535/52159615766_9eedbebb83_k.jpg",
     url: "https://calm-journey-01741.herokuapp.com/",
     code: "https://github.com/yoshino9397/TimeTracker-App",
   },
   {
     name: "Social Media App",
-    description: `This app is a social app with a chat function. It was built with MongoDB, express.js, node.js, React (MERN), and You can use the real-time chat function was equipped using SocketIO. You can follow posts with images and other users.`,
+    description: `This app is a social app with a chat function. It was built with MongoDB, express.js, node.js, and React (MERN), and You can use the real-time chat function equipped using SocketIO. You can follow posts with images and other users.`,
     image: "https://live.staticflickr.com/65535/52005056131_056d40957c_k.jpg",
     url: "https://yoshino-chatapp.herokuapp.com/",
     code: "https://github.com/yoshino9397/Realtime-ChatApp",
   },
   {
     name: "E-commerce App",
-    description: `The MERN app, which uses Redux, defines the handling of users and items in the cart. Stripe is also used for credit card payments.On the server-side, the password is encrypted with CryptoJS, the access token can be obtained with JWT.`,
+    description: `The MERN app, which uses Redux, defines the handling of users and items in the cart. Stripe is also used for credit card payments. On the server-side, the password is encrypted with CryptoJS, the access token can be obtained with JWT.`,
     image: "https://live.staticflickr.com/65535/52005120993_0969c57259_k.jpg",
     url: "https://deployretry.herokuapp.com/",
     code: "https://github.com/yoshino9397/Mern-EcommerceApp-Final",
   },
   {
     name: "Mystery Game Website",
-    description: `I built a Japanese-style mystery game site mainly with material UI and React, and focused on the design of details so that the worldview of the game can be easily conveyed to users. All the illustrations and backgrounds on the site were drawn by me using Clip Studio`,
+    description: `I built a Japanese-style mystery game site mainly with material UI and React, and focused on the design of details so that the worldview of the game can be easily conveyed to users. All the illustrations and backgrounds on the site were drawn by me using Clip Studio.`,
     image: "https://live.staticflickr.com/65535/52005121363_b9636ad538_k.jpg",
     url: "https://yoshino-mysterygame.netlify.app/",
     code: "https://github.com/yoshino9397/MysteryGame-Page",
   },
   {
     name: "Cafe Order App",
-    description: `Mainly used Next.js and React-paypal-js as payment method, also used Redux, Cloudinary.Created Responsive User Experience with Grids to see an organized cafe menu We developed a stylish cafe ordering app. New menus can be added from the admin panel.`,
+    description: `Mainly used Next.js and React-PayPal-js as payment methods, and also used Redux, and Cloudinary. Created Responsive User Experience with Grids to see an organized cafe menu We developed a stylish cafe ordering app. New menus can be added from the admin panel.`,
     image: "https://live.staticflickr.com/65535/52041459474_337371e876_k.jpg",
     url: "https://cafe-order-app.herokuapp.com/",
     code: "https://github.com/yoshino9397/Cafe-Order-Deploy",
   },
   {
     name: "Hotel Booking App",
-    description: `The booking application is designed to allow users to search for hotels that match their criteria by region, date and number of people, with reference to the design and structure of Booking.com. You can access the admin page from the username displayed after logging in.`,
+    description: `The booking application is designed to allow users to search for hotels that match their criteria by region, date, and number of people, with reference to the design and structure of Booking.com. You can access the admin page from the username displayed after logging in.`,
     image: "https://live.staticflickr.com/65535/52040166147_e698d3aea0_k.jpg",
     url: "https://yoshino-booking.herokuapp.com/",
     code: "https://github.com/yoshino9397/Booking-App",
   },
   {
     name: "Travel-Map App",
-    description: `The MERN map app for travel lovers who can pin their favorite places and memories on a simple map. Mapbox is used as the API, and the design is also original. On the server-side, it's built so that hash passwords can be generated using bcrypt.`,
+    description: `The MERN map app is for travel lovers who can pin their favorite places and memories on a simple map. Mapbox is used as the API, and the design is also original. On the server side, it's built so that hash passwords can be generated using bcrypt.`,
     image: "https://live.staticflickr.com/65535/52005056001_cacd2bb2eb_k.jpg",
     url: "https://yoshino-travelmap.herokuapp.com/",
     code: "https://github.com/yoshino9397/Mern-TravelApp",
@@ -85,7 +85,7 @@ const projects = [
   },
   {
     name: "Horror Game Website",
-    description: `Developed front-end user experience using HTML,SASS,Javascript and prepared some gimmicks so that users can experience the world view of the game from the site as well.Designed all the pictures on the site and drew them in Clip Studio.`,
+    description: `Developed front-end user experience using HTML, SASS, and Javascript and prepared some gimmicks so that users can experience the world view of the game from the site as well. Designed all the pictures on the site and drew them in Clip Studio.`,
     image: "https://live.staticflickr.com/65535/52005055841_ed8e5e2f56_k.jpg",
     url: "https://yoshino9397.github.io/whrereAreYou/HTML/top.html",
     code: "https://github.com/yoshino9397/whrereAreYou",
@@ -93,8 +93,7 @@ const projects = [
 
   {
     name: "Musician website",
-    description: `Developed with a focus on HTML, CSS, and Javascript, with easy-to-read and artist-like creative design in mind.Responsive design, changing the menu bar display on PC and smartphone, and creating website layout / user interface.
-    `,
+    description: `Developed with a focus on HTML, CSS, and Javascript, with easy-to-read and artist-like creative design in mind. Responsive design, changing the menu bar display on PC and smartphone, and creating website layout/user interface.`,
     image: "https://live.staticflickr.com/65535/52005584765_d990e1bde5_k.jpg",
     url: "https://yoshino9397.github.io/Ysinger/HTML/web.html",
     code: "https://github.com/yoshino9397/Ysinger",
